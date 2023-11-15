6b1edb6604ca05f218bad8c896df26f8f18a0a19
veronicamorelli <veronicamorelli99@yahoo.com>
2023-11-15 18:22:36 +0100

    created script to run model with  hs_nopca

---
+++ upernet_swin_pan.py
@@ -0,0 +1,84 @@
+# --------------------------------------------------------
+# Swin Transformer
+# Copyright (c) 2021 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ze Liu, Yutong Lin, Yixuan Wei
+# --------------------------------------------------------
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+from itertools import chain
+
+# from swin import SwinTransformer
+from swin_EF import SwinTransformer
+# from swin_TF import SwinTransformer
+from upernet import PSPModule, FPN_fuse
+
+import json
+with open('./config.json', 'r') as f:
+  config = json.load(f)
+
+class UperNet_swin(nn.Module):
+
+    # Implementing only the object path
+    
+    def __init__(self,
+                 params:dict, 
+                 num_classes=9): # c'erano altri parametri !
+        
+        super(UperNet_swin, self).__init__()
+
+        self.config = params 
+
+        self.backbone = SwinTransformer(pretrain_img_size= self.config["train_size"][0], 
+                                        patch_size=self.config["patch_size"],
+                                        rgb_chans=self.config["rgb_chans"], 
+                                        hs_chans=self.config["hs_chans_pan"], #182
+                                        dem_chans=self.config["dem_chans"],
+                                        embed_dim = self.config["embed_dim_T_S"],
+                                        depths = self.config["depth_S_B_L"],
+                                        num_heads = self.config["num_heads"],
+                                        window_size = self.config["window_size"],
+                                        mlp_ratio = self.config["mlp_ratio"],
+                                        qkv_bias=True,
+                                        qk_scale=None,
+                                        drop_rate= self.config["drop_rate"],
+                                        attn_drop_rate= self.config["attn_drop_rate"],
+                                        drop_path_rate= self.config["drop_path_rate"],
+                                        norm_layer=nn.LayerNorm,
+                                        ape=False,
+                                        patch_norm=True,
+                                        out_indices=(0, 1, 2, 3),
+                                        # frozen_stages=-1,
+                                        use_checkpoint=False)
+        
+        feature_channels= self.config["feature_channels_upernet_T_S"]
+        self.PPN = PSPModule(in_channels=feature_channels[-1])
+        self.FPN = FPN_fuse(feature_channels, fpn_out=feature_channels[0])
+        self.head = nn.Conv2d(feature_channels[0], num_classes, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        input_size = (self.config["train_size"][0], self.config["train_size"][1])
+
+        features = self.backbone(x)
+        # len(features) = 4
+        # features[0].shape = torch.Size([16, 96, 64, 64])
+        # features[1].shape = torch.Size([16, 192, 32, 32])
+        # features[2].shape = torch.Size([16, 384, 16, 16])
+        # features[3].shape = torch.Size([16, 768, 8, 8])
+        features[-1] = self.PPN(features[-1])
+        x = self.head(self.FPN(features)) # 16, 9, 64, 64
+
+        x = F.interpolate(x, size=input_size, mode='bilinear') # 16, 9, 256, 256
+        return x
+
+    def get_backbone_params(self):
+        return self.backbone.parameters()
+
+    def get_decoder_params(self):
+        return chain(self.PPN.parameters(), self.FPN.parameters(), self.head.parameters())
+
+    def freeze_bn(self):
+        for module in self.modules():
+            if isinstance(module, nn.BatchNorm2d): module.eval()
